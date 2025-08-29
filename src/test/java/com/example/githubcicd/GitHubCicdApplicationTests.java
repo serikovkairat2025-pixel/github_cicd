7d13128edd42c2ce902875270349dcb5dbf9bc87
Almanbet-Arstan Totoev <almanbet.totoev@gmail.com>
2025-08-29 14:11:03 +0600

    code

---
+++ src/test/java/com/example/githubcicd/GitHubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
